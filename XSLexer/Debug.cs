922ab68473e8705e61d9c5c6706616cbc00792ea
Coda <Errorcodebin_2@hotmail.com>
2019-04-11 15:33:33 +0200

    Added a static debug class
    UX, hurray.

---
+++ XSLexer/Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace XSLexer
+{
+    static class Debug
+    {
+        static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Log: " + message);
+        }
+
+        static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Err: " + message);
+        }
+
+        static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("War: " + message);
+        }
+
+    }
+}
