aae62057974b0d1799b4d1c376a4777797fff9e4
Coda <Errorcodebin_2@hotmail.com>
2019-04-11 11:14:09 +0200

    Initial project commit

---
+++ XSLexer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XSLexer
+{
+    class Program
+    {
+        // Main entry point
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to the Leftovers XS Lexer!");
+            Console.ReadKey();
+        }
+    }
+}
