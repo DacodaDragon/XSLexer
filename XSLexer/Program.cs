39a33cbf2919faacf5b9a0a811e619afeaf9925b
Coda <Errorcodebin_2@hotmail.com>
2019-04-15 21:38:24 +0200

    Removed Program Code

--- XSLexer/Program.cs
+++ XSLexer/Program.cs
@@ -9,33 +9,7 @@ namespace XSLexer
     {
         static void Main(string[] args)
         {
-            LexConfig config = new LexConfig(
-                new DataSet[] { DataSetParser.Parse("TokensDefinitions", Resources.TokenDefinitions) },
-                new DataSet[] { DataSetParser.Parse("DataType Words", Resources.DataTypes) });
-
-            TokenSet tokenSet = new Lexer(config).Lex(Resources.XsFunction);
-
-            Debug.Imp("STATS:");
-            Debug.Imp($"Contains {tokenSet.Length} tokens!");
-            for (int i = 0; i < config.Tokens.All.Length; i++)
-            {
-                if (tokenSet.GetAllOfType(config.Tokens.All.GetSet(i).Name, out TokenSet set))
-                {
-                    Debug.Imp($"Contains type [{config.Tokens.All.GetSet(i).Name}] {set.Length} times!");
-                }
-            }
-
-
-            Debug.Log("Tokens found: " + tokenSet.Length);
-            //Debug.Log(tokenSet);
-
-            StringBuilder sb = new StringBuilder(Resources.XsFunction.Length);
-            for (int i = 0; i < tokenSet.Length; i++)
-            {
-                sb.Append(tokenSet[i].Value);
-            }
-            File.WriteAllText("D:/Temp/Ree.txt", sb.ToString());
-            Debug.Pause();
+        
         }
     }
 }
